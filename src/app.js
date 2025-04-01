3f3f1686b9581e7f2b156bb20a75db7b571747e2
BuilderPrid <priyanshudwivedi004@gmail.com>
2025-04-01 10:04:03 +0530

    add cors suport

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express, { json } from 'express';
 import userRouter from '../routes/userRoutes.js'; // Import the userRouter
-const cors = require("cors");
+import cors from 'cors';
 const app = express();
 
 app.use(json());
