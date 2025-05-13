558a8b4beaa50d3adc7a95307f90ba16d125c506
PanwarPranav <pranavpanwar15may@gmail.com>
2025-05-13 21:49:34 +0530

    Admin mei app daal diya

--- gail_admin_app/gail_admin/ios/Runner/AppDelegate.swift
+++ gail_admin_app/gail_admin/ios/Runner/AppDelegate.swift
@@ -1,13 +1,14 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.process.env.GOOGLE_MAPS_API_KEY
+    GMSServices.provideAPIKey("AIzaSyAjGrkR7nYPDufrt68_Cgk4ljZYKeMmqpE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
